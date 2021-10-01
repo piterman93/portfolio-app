a0e45f72e70d51e086c7633087cada63adeb2c0c
Piotr Maniak <piotrmaniak93@gmail.com>
2021-10-01 10:52:22 +0200

    added home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,13 @@
-import React from "react";
-
 import "../styles/Home.scss";
 
 export default function Home() {
   return (
     <div className="home" id="home">
-      Home
+      <div className="wrapper">
+        <h1>Hey! I am Piotrek.</h1>
+        <h2>I design and create web applications.</h2>
+        <h3>Feel free to scroll down and explore more about me.</h3>
+      </div>
     </div>
   );
 }
--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -1,4 +1,9 @@
 import React from "react";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import InstagramIcon from "@mui/icons-material/Instagram";
+
 import "../styles/SideMenu.scss";
 
 export default function SideMenu({ showHeaderMenu, showMenuHandler }) {
@@ -21,7 +26,29 @@ export default function SideMenu({ showHeaderMenu, showMenuHandler }) {
           </li>
         </ul>
       </div>
-      <div className="menu__bottom">ikony</div>
+
+      <div className="menu__bottom">
+        <div className="icon_wrapper">
+          <a href="https://github.com/piterman93" target="_blank">
+            <GitHubIcon />
+          </a>
+        </div>
+        <div className="icon_wrapper">
+          <a href="https://www.linkedin.com/in/piotrmaniak93/" target="_blank">
+            <LinkedInIcon />
+          </a>
+        </div>
+        <div className="icon_wrapper">
+          <a href="https://www.facebook.com/piotrek.maniak" target="_blank">
+            <FacebookIcon />
+          </a>
+        </div>
+        <div className="icon_wrapper">
+          <a href="https://www.instagram.com/piterman93/" target="_blank">
+            <InstagramIcon />
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
