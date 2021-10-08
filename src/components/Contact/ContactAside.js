0b85069da8b12384088fe1ff6c817245520e6940
Piotr Maniak <piotrmaniak93@gmail.com>
2021-10-08 13:08:33 +0200

    page completed - portfolio to udpate

--- src/components/Contact/ContactAside.js
+++ src/components/Contact/ContactAside.js
@@ -8,21 +8,20 @@ import myLogo from "../../assets/logo-transparent.png";
 export default function ContactAside() {
   return (
     <div className="contact__aside">
-      <div className="wrapper">
+      <div className="wrapper__aside">
         <h2>Get in touch!</h2>
-
         <p>
           Would you like to ask about cooperation or provide your feedback about
           page?{" "}
         </p>
         <p>Do not hesitate and contact me by filling the form!</p>
-        <div className="icons_wrapper">
-          <div className="logo">
-            <img src={contactLogo} alt="messenger logo icon" />
-          </div>
-          <div className="logo my_logo">
-            <img src={myLogo} alt="Piotr Maniak logo icon" />
-          </div>
+      </div>
+      <div className="icons_wrapper">
+        <div className="logo">
+          <img src={contactLogo} alt="messenger logo icon" />
+        </div>
+        <div className="logo my_logo">
+          <img src={myLogo} alt="Piotr Maniak logo icon" />
         </div>
       </div>
     </div>
